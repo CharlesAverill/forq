4ffbe221d3e649e6781e3773433ea9291daed237
Charles Averill <charlesaverill20@gmail.com>
2025-08-25 13:27:57 -0500

    Remove makefile.d

--- .Makefile.d
+++
@@ -1,3 +0,0 @@
-theories/Test.vo theories/Test.glob theories/Test.v.beautified theories/Test.required_vo: theories/Test.v 
-theories/Test.vio: theories/Test.v 
-theories/Test.vos theories/Test.vok theories/Test.required_vos: theories/Test.v 
